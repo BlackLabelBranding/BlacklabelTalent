fd6f354d2655168cbdd82c46b1beaf15fd12d7de
BlackLabelBranding <lance@garzaunlimited.com>
2026-06-23 21:36:53 -0500

    Disable stale service worker cache

--- sw.js
+++ sw.js
@@ -1,40 +1,23 @@
-const CACHE_NAME = "blacklabel-talent-v7";
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./favicon.ico",
-  "./manifest.webmanifest",
-  "./icons/favicon-16.png",
-  "./icons/favicon-32.png",
-  "./icons/icon-192.png",
-  "./src/app.js?v=20260624c",
-  "./src/styles.css?v=20260624c",
-  "./src/brand.css?v=20260624c",
-  "./src/live.css?v=20260624c",
-  "./src/app-polish.css?v=20260624c",
-  "./src/data/mockData.js",
-  "./src/lib/hubApi.js?v=20260624b"
-];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
   self.skipWaiting();
+  event.waitUntil(Promise.resolve());
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
+      .then(() =>
+        self.clients.matchAll({ type: "window" }).then((clients) => {
+          clients.forEach((client) => client.navigate(client.url));
+        })
+      )
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request).then((cached) =>
-      cached || fetch(event.request).catch(() => caches.match("./index.html"))
-    )
-  );
+  event.respondWith(fetch(event.request));
 });
